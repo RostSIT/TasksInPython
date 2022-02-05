db508a53aafe5921c828f77375758dd52f381e14
RostSIT <streltsovrp@gmail.com>
2022-02-05 19:41:07 +0200

    Зачет

--- 5.6 Вложенные списки 1 из 16 шагов пройден 0 из 87 баллов  получено/56 12 16 Состязания - 3, вывести одно число - номер победителя..py
+++ 5.6 Вложенные списки 1 из 16 шагов пройден 0 из 87 баллов  получено/56 12 16 Состязания - 3, вывести одно число - номер победителя..py
@@ -31,3 +31,28 @@ Sample Output:
 
 0
 """
+
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for i in range(n)]
+
+max_score = 0
+max_summa = 0
+max_index = 0
+for row in range(n):
+    max_try = 0
+    s = 0
+    for column in range(m):
+        s += a[row][column]
+        if a[row][column] > max_try:
+            max_try = a[row][column]
+
+    if max_try > max_score:
+        max_score = max_try
+        max_summa = s
+        max_index = row
+    elif max_try == max_score and s > max_summa:
+        max_score = max_try
+        max_summa = s
+        max_index = row
+print(max_index)
+
