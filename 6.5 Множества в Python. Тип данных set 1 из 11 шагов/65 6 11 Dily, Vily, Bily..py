41917d0a8d976a160960ab061dd225542e3bbf80
RostSIT <streltsovrp@gmail.com>
2022-02-15 14:57:53 +0200

    Проработать

--- 6.5 Множества в Python. Тип данных set 1 из 11 шагов/65 6 11 Dily, Vily, Bily..py
+++ 6.5 Множества в Python. Тип данных set 1 из 11 шагов/65 6 11 Dily, Vily, Bily..py
@@ -1,7 +1,14 @@
-a = input()
-b = {}
-while a == 'конец':
-    a = input()
-    b.setdefault(a)
 
-print(b)
+a = True
+resolt = {'Били': set(), 'Вили': set(), 'Дили': set()}
+while a == True:
+    vvod = input()
+    if vvod == 'конец':
+        a = False
+    else:
+        name, value = vvod.split(': ')
+        resolt[name].add(value)
+for i in resolt:
+    resolt[i] = len(resolt[i])
+for i in sorted(resolt.items(), key=lambda para: -para[1]):
+    print('Количество уникальных комментаторов у', i[0], '-', i[1])
