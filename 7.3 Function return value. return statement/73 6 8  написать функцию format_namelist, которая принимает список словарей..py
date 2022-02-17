a524a7f47bb3a0066dd38b9c120234830f762857
RostSIT <streltsovrp@gmail.com>
2022-02-17 17:04:23 +0200

    Зачет

--- 7.3 Function return value. return statement/73 6 8  написать функцию format_namelist, которая принимает список словарей..py
+++ 7.3 Function return value. return statement/73 6 8  написать функцию format_namelist, которая принимает список словарей..py
@@ -1,43 +1,79 @@
-def format_namelist(dictionary):
-    string = ''
-    d = []
-    a = ' и '
-    c = ', '
-    for i in range(len(dictionary)):
-        d.append(dictionary[i]['name'])
-    if len(d) == 3:
-        for i in range(len(d) - 1):
-            string += str(d[i])
-            c.join(string)
-        string += a + str(d[2])
-        return string
-
-    if len(d) == 2:
-        string = a.join(d)
-        return string
-    if len(d) == 1:
-        return string.join(d)
-    if len(d) == 0:
-        return string
+def format_namelist(x):
+    nameLIST = []
+    finishList = []
+    stoka = ''
+    for i in range(len(x)):
+        nameLIST.append(x[i]['name'])
+    if len(nameLIST) == 0:
+        return stoka
+    elif len(nameLIST) == 1:
+        for i in nameLIST:
+            stoka = i
+        return stoka
+    else:
+        for i in range(len(nameLIST)):
+            if i == len(nameLIST):
+                finishList.append(nameLIST[i])
+            elif i == len(nameLIST) - 1:
+                finishList.append('и')
+                finishList.append(nameLIST[i])
+            elif i == len(nameLIST) - 2:
+                finishList.append(nameLIST[i])
+            else:
+                finishList.append(nameLIST[i] + ',')
+    for i in finishList:
+        stoka += i + ' '
+    stoka = stoka[:-1]
+    return stoka
 
 
 print(format_namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]))
 print(format_namelist([{'name': 'Bart'}, {'name': 'Lisa'}]))
 print(format_namelist([{'name': 'Bart'}]))
 print(format_namelist([]))
-# for key, value in dictionary.items():
 
 
-#         if len(dictionary) == 3:
-#             string+= value
-#         if len(dictionary) == 2:
-#             string += dictionary[0] + a + dictionary[1]
-#         if len(dictionary) == 1:
-#             string += dictionary[0]
-#     return string
-#
-#
-# print(format_namelist({'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}))
-# print(format_namelist({'name': 'Bart'}, {'name': 'Lisa'}))
-# print(format_namelist({'name': 'Bart'}))
-format_namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}])
+def format_namelist(dy):
+    return ' и '.join(i['name'] for i in dy).replace(' и', ',', len(dy) - 2)
+
+
+print(format_namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]))
+print(format_namelist([{'name': 'Bart'}, {'name': 'Lisa'}]))
+print(format_namelist([{'name': 'Bart'}]))
+print(format_namelist([]))
+
+
+def format_namelist(lst):
+    a = []
+    b = []
+    c = []
+    for i in range(len(lst)):
+        a.append(lst[i]['name'])
+    if len(a) > 1:
+        a.append(a.pop(-2) + " и " + a.pop())
+    return ', '.join(a)
+
+
+print(format_namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'},  {'name': 'Maie'}]))
+print(format_namelist([{'name': 'Bart'}, {'name': 'Lisa'}]))
+print(format_namelist([{'name': 'Bart'}]))
+print(format_namelist([]))
+
+"""7.3 Возвращаемое значение функции. Оператор return
+6 из 8 шагов пройдено
+9 из 18 баллов  получено
+Ваша задача написать функцию format_namelist, которая принимает список словарей, у каждого словаря в списке есть только ключ name
+
+Функция format_namelist должна вернуть отформатированную строку, в которой все имена из списка разделяются запятой кроме последних двух имен, они должны быть разделены союзом "и". Если в списке нет ни одного имени, функция должна вернуть пустую строку. Ниже представлены примеры:
+
+format_namelist([ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ])
+# returns 'Bart, Lisa и Maggie'
+
+format_namelist([ {'name': 'Bart'}, {'name': 'Lisa'} ])
+# returns 'Bart и Lisa'
+
+format_namelist([ {'name': 'Bart'} ])
+# returns 'Bart'
+
+format_namelist([])
+# returns ''"""
---
+++ 7.3 Function return value. return statement/73 7 8 domain_name, которая принимает строку url, извлекает из нее доменное имя и возвращает его в качестве строки..py
@@ -0,0 +1,20 @@
+def domain_name(x):
+    y=[]
+    if '//' in x:
+        x.split('//')
+        x.split('.')
+        for i in (x):
+            y.append(i)
+            return y
+        else:
+            x.split('.')
+            #y.append(i)
+    return y
+
+
+print(domain_name("http://google.com"))
+print(domain_name("http://google.co.jp"))
+print(domain_name("www.xakep.ru"))
+print(domain_name("https://youtube.com"))
+print(domain_name("https://www.asos.com"))
+print(domain_name("http://www.lenovo.com"))
